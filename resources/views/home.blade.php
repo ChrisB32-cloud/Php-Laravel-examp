9e2873aa9a7818a59afa966390cc152f229df05d
ChrisBrownTheGuy <cbrowndev88@gmail.com>
2025-01-24 14:35:55 -0600

    inital commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,20 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <div class="border-[3px] border-black border-solid">
+        <h2>Register</h2>
+        <form action="/register" method="post">
+            @csrf
+            <input type="text" placeholder="name">
+            <input type="email" placeholder="email">
+            <input type="password" placeholder="password">
+            <button>Register</button>
+        </form>
+    </div>
+ 
+</body>
+</html>
