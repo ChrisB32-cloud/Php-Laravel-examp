922b09c8fc08124428c776030f976ac507c1c8ab
ChrisBrownTheGuy <cbrowndev88@gmail.com>
2025-01-31 09:22:17 -0600

    Post form styled

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -9,28 +9,45 @@
 
 <body>
     <div class="flex flex-col justify-center min-h-full px-6 py-12 lg:px-8">
-        <div class="sm:mx-auto sm:w-full sm:max-w-sm">
-            <img class="w-auto h-10 mx-auto" src="https://tailwindui.com/plus/img/logos/mark.svg?color=indigo&shade=600"
-                alt="Your Company">
-            <h2 class="mt-10 font-bold tracking-tight text-center text-gray-900 text-2xl/9">Sign in to your account</h2>
-        </div>
+
 
         @auth
-            <p>Congratulations, you are logged in!</p>
-            <form action="/logout" method="POST">
-                @csrf
-                <button>Logout</button>
-            </form>
-            <div class="text-indigo-500"
-                style="display: flex; flex-direction:column; justify-content: center; align-items: center;">
-                <h2>Create a new post</h2>
-                <form action="/create-post" method="post"
-                    style="display: flex; flex-direction:column; justify-content: center; align-items: center;">
+            <div class="sm:mx-auto sm:w-full sm:max-w-sm">
+                <img class="w-auto h-10 mx-auto" src="https://tailwindui.com/plus/img/logos/mark.svg?color=indigo&shade=600"
+                    alt="Your Company">
+                <h2 class="mt-10 font-bold tracking-tight text-center text-gray-900 text-2xl/9">Congratulations, you are
+                    logged in!</h2>
+
+                <form action="/logout" method="POST" class="space-y-6">
                     @csrf
-                    <input name="title" type="text" placeholder="title">
-                    <textarea name="body" placeholder="content"></textarea>
-                    <button>Save post</button>
+                    <button
+                        class="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm/6 font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Logout</button>
                 </form>
+                <div class="p-2 text-indigo-500">
+                    <h2>Create a new post</h2>
+                    <form action="/create-post" method="post" class="space-y-6">
+                        @csrf
+                        <div>
+                            <label for="title" class="block font-medium text-gray-900 text-sm/6">Title</label>
+                            <div class="mt-2">
+                                <input name="title" type="text" placeholder="title"
+                                    class="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6">
+                            </div>
+                        </div>
+                        <div>
+                            <label for="post" class="block font-medium text-gray-900 text-sm/6">Post</label>
+                            <div class="mt-2">
+                                <textarea name="body" rows="3" placeholder="content"
+                                    class="block w-full resize-none bg-transparent outline-gray-300 px-3 py-1.5 text-base text-gray-900 placeholder:text-gray-400 focus:outline focus:outline-0 sm:text-sm/6"></textarea>
+                            </div>
+                        </div>
+                        <div>
+                            <button
+                                class="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm/6 font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Save
+                                post</button>
+                        </div>
+                    </form>
+                </div>
             </div>
             <div class="border-2 border-indigo-500"
                 style="display: flex; flex-direction:column; justify-content: center; align-items: center;">
@@ -47,6 +64,11 @@
                 @endforeach
             </div>
         @else
+            <div class="sm:mx-auto sm:w-full sm:max-w-sm">
+                <img class="w-auto h-10 mx-auto" src="https://tailwindui.com/plus/img/logos/mark.svg?color=indigo&shade=600"
+                    alt="Your Company">
+                <h2 class="mt-10 font-bold tracking-tight text-center text-gray-900 text-2xl/9">Sign in to your account</h2>
+            </div>
             <div class="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
                 <h2>Register</h2>
                 <form action="/register" method="post" class="space-y-6">
